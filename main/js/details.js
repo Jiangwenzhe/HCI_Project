632488002b5092abab6d5b228b3caca696590c65
Erzhuyijian <1821344102@qq.com>
2018-12-20 21:36:58 +0800

    12/20

--- main/js/details.js
+++ main/js/details.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
    var url = window.location.search;
-   if(url.indexOf("?")!= -1) {
-    var searchValue = url.substr(url.indexOf("=")+1);
-    console.log(searchValue);
-
-    $('#model-obj').attr('src',`source/${searchValue}/${searchValue}.obj`);
-    $('#model-mtl').attr('src',`source/${searchValue}/${searchValue}.mtl`);
-}
+   var value = url.split('?')[1];
+   var ModelID = value.split('&')[0];
+   var size = value.split('&')[1];
+   console.log(size)
+    
+    $('#model-obj').attr('src',`source/${ModelID}/${ModelID}.obj`);
+    $('#model-mtl').attr('src',`source/${ModelID}/${ModelID}.mtl`);
+    var scaleObj = {
+        x: size,
+        y: size,
+        z: size
+    }
+    document.querySelector('a-obj-model').setAttribute('scale',scaleObj)
+    console.log(`${size} ${size} ${size}`)
+    console.log(document.querySelector('a-obj-model').getAttribute('scale'))
 })
--- main/js/index.js
+++ main/js/index.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
   $('.card').mouseover(function() {
-      $(this).addClass('animated pulse faster');
+      $(this).addClass('animated pulse fast');
   }).mouseleave(function() {
-    $(this).removeClass('animated pulse faster');
+    $(this).removeClass('animated pulse fast');
   })
+
+  $('#title').mouseover(function() {
+    $(this).addClass('animated jello ');
+}).mouseleave(function() {
+  $(this).removeClass('animated jello ');
+})
 });
 
 $('.jump-button').click(function() {
-  var value = $(this).attr('id')
-  window.location.href = `details.html?value=${value}`
+  var modelID = $(this).attr('id');
+ $('#submitModel').click(function() {
+  var zoomSize = $('#zoomSize').val();
+  window.location.href = `details.html?${modelID}&${zoomSize}`
+  console.log(`details.html?value=${modelID}&size=${zoomSize}`)
+ })
 })
+
