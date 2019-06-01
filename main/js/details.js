bbe25020778c3939e06d03b9b6c39186e439804d
Erzhuyijian <1821344102@qq.com>
2019-06-01 10:03:56 +0800

    添加交互，支持放大，缩小、移动的功能

--- main/js/details.js
+++ main/js/details.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
    var value = url.split('?')[1];
    var ModelID = value.split('&')[0];
    var size = value.split('&')[1];
-   console.log(size)
-    
+   console.log(ModelID)
+    console.log(document.querySelector('a-marker').setAttribute('Url', `source/${ModelID}/pattern-${ModelID}.patt`))
     $('#model-obj').attr('src',`source/${ModelID}/${ModelID}.obj`);
     $('#model-mtl').attr('src',`source/${ModelID}/${ModelID}.mtl`);
+    console.log(document.querySelector('a-marker').getAttribute('Url'))
     var scaleObj = {
         x: size,
         y: size,
@@ -14,5 +15,70 @@ $(document).ready(function() {
     }
     document.querySelector('a-obj-model').setAttribute('scale',scaleObj)
     console.log(`${size} ${size} ${size}`)
-    console.log(document.querySelector('a-obj-model').getAttribute('scale'))
+
+
+
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const up = document.querySelector("#up");
+        const down = document.querySelector('#down')
+        const big = document.querySelector('#big');
+        const small = document.querySelector('#small')
+        const left = document.querySelector('#left')
+        const right = document.querySelector('#right')
+        const aEntity = document.querySelector("#animated-model");
+
+
+        up.addEventListener('mousedown', function(ev, target){
+                const rotation = aEntity.getAttribute('rotation');
+                    rotation.x +=10
+                    console.log(rotation )
+                    aEntity.setAttribute('rotation', rotation);
+
+        });
+
+        down.addEventListener('mousedown', function(ev, target){
+                const rotation = aEntity.getAttribute('rotation');
+                rotation.x -=10
+                console.log(rotation )
+                aEntity.setAttribute('rotation', rotation);
+
+        });
+
+        left.addEventListener('mousedown', function(ev, target){
+                const rotation = aEntity.getAttribute('rotation');
+                rotation.y -=10
+                console.log(rotation )
+                aEntity.setAttribute('rotation', rotation);
+
+        });
+
+        right.addEventListener('mousedown', function(ev, target){
+            const rotation = aEntity.getAttribute('rotation');
+            rotation.y +=10
+            console.log(rotation )
+            aEntity.setAttribute('rotation', rotation);
+
+        });
+
+        big.addEventListener('click', function(ev, target){
+            console.log(1)
+                const scale= aEntity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.001);
+                console.log(scale )
+                aEntity.setAttribute('scale', scale);
+
+        });
+
+        small.addEventListener('click', function(ev, target){
+            console.log(1)
+                const scale= aEntity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] - 0.001);
+                console.log(scale )
+                aEntity.setAttribute('scale', scale);
+        });
+
+}});
+
 })
